088893c3e340b1c4c5b5e3fd89812bb360b2fe8f
ViniciusAClement <clementevinicius13@gmail.com>
2025-11-21 02:22:20 -0300

    spring initializr

---
+++ src/main/java/com/bcc/cca/CcaApplication.java
@@ -0,0 +1,13 @@
+package com.bcc.cca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcaApplication.class, args);
+	}
+
+}
